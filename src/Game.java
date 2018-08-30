b02223b60f43dfdec074ebc4bac51335d9cdeed8
bluevelvet <folkjoker777@gmail.com>
2018-08-30 14:11:25 +0700

    Finish before checkwin

---
+++ src/Game.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Game {
+    private static OX ox;
+    private static Scanner kb = new Scanner(System.in);
+    private static int col;
+    private static int row;
+
+    public static void main(String[] args) {
+        ox = new OX();
+        while(true){
+            printTable();
+            input();
+            ox.put(col,row);
+            ox.switchPlayer();
+        }
+    }
+
+    private static void input() {
+        boolean canPut = true;
+        do {
+            System.out.print(ox.getCurrentPlayer() + " Col :");
+            col = kb.nextInt();
+            System.out.print(ox.getCurrentPlayer() + " Row :");
+            row = kb.nextInt();
+            canPut = ox.put(col, row);
+            if (!canPut) {
+                System.out.println("Please input number between 0-2");
+            }
+        }while(!canPut);
+    }
+
+    private static void printTable() {
+        System.out.print(ox.getTableString());
+    }
+}
